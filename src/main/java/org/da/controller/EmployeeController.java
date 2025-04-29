eab90e92ed54e824bcba9d797d88c5734575d934
dineshalahakoon <dineshroxx1997@gmail.com>
2025-04-29 10:00:06 +0530

    create employee controller

--- src/main/java/org/da/controller/EmployeeController.java
+++ src/main/java/org/da/controller/EmployeeController.java
@@ -1,5 +1,31 @@
 package org.da.controller;
 
+import org.da.dto.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@RequestMapping("employees")
+
 public class EmployeeController {
 
+    @PostMapping("/add-employee")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void add(@RequestBody Employee employe1){
+
+    }
+
+    @GetMapping("/get-all")
+    public void getall(){
+
+    }
+
+    @DeleteMapping("/delete-emp/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deleteEmployee(@PathVariable String id) {
+
+    }
+    @PutMapping("/update-employee")
+    public void updateEmployee(@RequestBody Employee employe1){
+
+    }
 }
