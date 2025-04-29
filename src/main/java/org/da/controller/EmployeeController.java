44427323d76a47fd39576e1737d43524256a297f
dineshalahakoon <dineshroxx1997@gmail.com>
2025-04-29 09:55:54 +0530

    create employee dto

---
+++ src/main/java/org/da/controller/EmployeeController.java
@@ -0,0 +1,5 @@
+package org.da.controller;
+
+public class EmployeeController {
+
+}
---
+++ src/main/java/org/da/dto/Employee.java
@@ -0,0 +1,15 @@
+package org.da.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String createAt;
+    private String UplordAt;
+
+}
